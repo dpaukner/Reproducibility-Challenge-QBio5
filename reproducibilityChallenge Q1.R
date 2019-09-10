a719e71ec346848d635fb1b213c8d154ab711a0e
Yichen Liu <yichenliu@uchicago.edu>
2019-09-10 17:33:02 -0400

    Q1 Working

--- reproducibilityChallenge Q1.R
+++ reproducibilityChallenge Q1.R
@@ -1,4 +1,30 @@
 library(tidyverse)
-setwd("~/Documents/BSD-QBio5/tutorials/reproducibility/data")
-arth <- read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
-weev_data <- read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
+library(ggplot2)
+library(RMKdiscrete)
+setwd("~/Desktop/BSD-QBio5/tutorials/reproducibility/data")
+arthData <-
+  read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
+spiderData <-
+  arthData[, c("k_number_of_arthropods",
+               "C_count_of_boards_with_k_spiders")]
+spiderSum <-
+  spiderData$k_number_of_arthropods * spiderData$C_count_of_boards_with_k_spiders
+spiderSum <- sum(spiderSum)
+countSum <- sum(spiderData$C_count_of_boards_with_k_spiders)
+spiderMean <- spiderSum / countSum
+
+k <- 0:17
+realData <-
+  (
+    spiderData$C_count_of_boards_with_k_spiders / sum(spiderData$C_count_of_boards_with_k_spiders)
+  )
+pDistriData <- dpois(k, spiderMean)
+LGPData <- dLGP(k, spiderMean, 0)
+df <- data.frame(k, pDistriData, realData, LGPData)
+
+g <- ggplot(df, aes(k)) +
+  geom_point(aes(y = realData), colour = "green") +  # first layer
+  geom_line(aes(y = pDistriData), colour = "red") + # second layer
+  geom_line(aes(y = LGPData), colour = "yellow") # third layer
+g <- g + ylab("Probability") + xlab("Number of Arthropods") 
+g

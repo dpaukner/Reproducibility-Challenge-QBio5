9c036797a7da92655f15fd43c9495fa57fc9f38a
wlisicka <wlisicka@uchicago.edu>
2019-09-10 17:18:17 -0400

    plot with labelled axis

--- reproducibilityChallenge Q2.R
+++ reproducibilityChallenge Q2.R
@@ -1,12 +1,10 @@
 library(tidyverse)
-#set your own directory here
-setwd("~/BSD-QBio5/tutorials/reproducibility/data")
-
+setwd("~/BSD-QBio5/tutorials/reproducibility/data") #set your own directory here
 arth <- read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
 k <- arth[,1]
 sowbug_counts <- arth[,3]
 mean_sowbug <- (k*sowbug_counts)/sum(sowbug_counts)
 pois_sowbug <- dpois(k, mean_sowbug, log = FALSE)
 norm_sow <- sowbug_counts/sum(sowbug_counts)
-plot(k,norm_sow, col = "red")
+plot(k,norm_sow, col = "red", xlab = "Number of Sowbugs", ylab = "Probability")
 lines(k, pois_sowbug, col = "green")

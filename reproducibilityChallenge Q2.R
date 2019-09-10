8a25446e8a26dc31ef4d2bef937e3f170e2868a5
Dawn Paukner <dawnpaukner@gmail.com>
2019-09-10 14:50:10 -0500

    FOund sowbug mean and deleted unnecessary file

--- reproducibilityChallenge Q2.R
+++ reproducibilityChallenge Q2.R
@@ -1,4 +1,10 @@
 library(tidyverse)
-setwd("~/Documents/BSD-QBio5/tutorials/reproducibility/data")
+#set your own directory here
+setwd("~/BSD-QBio5/tutorials/reproducibility/data")
+
 arth <- read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
-weev_data <- read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
+
+sowbug_counts <- arth[,3]
+mean_sowbug <- mean(sowbug_counts)
+
+

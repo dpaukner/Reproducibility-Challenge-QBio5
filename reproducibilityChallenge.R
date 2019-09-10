7ac87d74af652900dd404b17dc6bc30e4eece782
dpaukner <53882279+dpaukner@users.noreply.github.com>
2019-09-10 12:45:27 -0500

    Added initial Rscript

---
+++ reproducibilityChallenge.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+arth_data <- read_csv("./BSD-QBio5/tutorials/reproducibility/data/cole_arthropod_data_1946.csv")
+weev_data <- read_csv("./BSD-QBio5/tutorials/reproducibility/data/mitchell_weevil_egg_data_1975.csv")

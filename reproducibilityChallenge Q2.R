13077a458e86426d4b0be1986cf524bb4ad9daa1
Dawn Paukner <dawnpaukner@gmail.com>
2019-09-10 15:47:39 -0500

    Actually right this time

--- reproducibilityChallenge Q2.R
+++ reproducibilityChallenge Q2.R
@@ -5,7 +5,7 @@ setwd("~/BSD-QBio5/tutorials/reproducibility/data")
 arth <- read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
 k <- arth[,1]
 sowbug_counts <- arth[,3]
-mean_sowbug <- mean(sowbug_counts)
+mean_sowbug <- k*sowbug_counts/sum(sowbug_counts)
 
 pois_sowbug <- dpois(k, mean_sowbug, log = FALSE)
 

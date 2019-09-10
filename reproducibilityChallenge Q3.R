9fe19ab01184bb103cf24d04795c02ab682d3db6
bnbanks2 <55111925+bnbanks2@users.noreply.github.com>
2019-09-10 17:29:16 -0400

    Updated Reproducibility Challenge 3

--- reproducibilityChallenge Q3.R
+++ reproducibilityChallenge Q3.R
@@ -1,4 +1,14 @@
-library(tidyverse)
-setwd("~/Documents/BSD-QBio5/tutorials/reproducibility/data")
-arth <- read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
-weev_data <- read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
+library(tidyverse) # calls 
+
+bugsdoc <- read.csv("~/Desktop/BSD-QBio5/tutorials/reproducibility/data/cole_arthropod_data_1946.csv")
+weevilsdoc <- read.csv("~/Desktop/BSD-QBio5/tutorials/reproducibility/data/mitchell_weevil_egg_data_1975.csv")
+
+lamb <- sum((weevilsdoc$k_number_of_eggs*weevilsdoc$C_count_of_beans_with_k_eggs)/sum(weevilsdoc$C_count_of_beans_with_k_eggs))
+
+
+x <- weevilsdoc$k_number_of_eggs
+
+plot(x,(weevilsdoc$C_count_of_beans_with_k_eggs)/sum(weevilsdoc$C_count_of_beans_with_k_eggs),xlab = 'Egg number (integer)',ylab = 'Proportion of beans with egg number x', col="red")
+lines(x,dpois(x,lamb),col="blue")
+
+

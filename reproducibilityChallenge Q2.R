7c9c7c77a47811a599cee62a65107b358b284b62
wlisicka <wlisicka@uchicago.edu>
2019-09-10 16:13:17 -0400

    Poisson distribution

--- reproducibilityChallenge Q2.R
+++ reproducibilityChallenge Q2.R
@@ -3,8 +3,12 @@ library(tidyverse)
 setwd("~/BSD-QBio5/tutorials/reproducibility/data")
 
 arth <- read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
-
+k <- arth[,1]
 sowbug_counts <- arth[,3]
 mean_sowbug <- mean(sowbug_counts)
+
+pois_sowbug <- dpois(k, mean_sowbug, log = FALSE)
+
+plot(pois_sowbug)
 
 

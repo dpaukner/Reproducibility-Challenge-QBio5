3d92f8fd9f9aee2dffe29e3c686abb21579ad6b2
Nicholeyang0215 <40403049+Nicholeyang0215@users.noreply.github.com>
2019-09-10 16:47:04 -0400

    Update reproducibilityChallenge Q1.R

--- reproducibilityChallenge Q1.R
+++ reproducibilityChallenge Q1.R
@@ -1,4 +1,15 @@
-library(tidyverse)
-setwd("~/Documents/BSD-QBio5/tutorials/reproducibility/data")
-arth <- read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
-weev_data <- read.csv("cole_arthropod_data_1946.csv", stringsAsFactors = FALSE)
+setwd("/Users/nicholeyang/Desktop/UChicago/Bootcamp/BSD-QBio5-master/tutorials/reproducibility/data")
+dt = read.csv("cole_arthropod_data_1946.csv")
+
+
+# emprical prob
+prob = dt$C_count_of_boards_with_k_spiders/sum(dt$C_count_of_boards_with_k_spiders)
+spider_mean = sum(dt$k_number_of_arthropods * dt$C_count_of_boards_with_k_spiders)/sum(dt$C_count_of_boards_with_k_spiders)
+
+## theoretical probability 
+x = c(0:17)
+y = dpois(dt$k_number_of_arthropods, lambda = spider_mean)
+
+plot(dt$k_number_of_arthropods, prob, xlab = "number of arthropods", ylab = "probability", main = 'Theoretical vs. Emprical')
+lines(x,y)
+
